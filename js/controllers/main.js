71f3cc217938466d897b7fa1ff284d9ed4de4e32
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-10-30 03:54:26 -0300

    Adding input directive

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,11 +1,42 @@
 angular.module('big-coach', [])
 
-.controller('MainCtrl', function() {
-  this.frame = function() {
+.controller('MainCtrl', function($scope, $sce) {
+  $scope.videoUrl = 'videos/ciudad.mp4';
+  $scope.video = null;
+
+  $scope.frame = function() {
     return videoFrame.get();
   };
 
-  this.smpte = function() {
+  $scope.smpte = function() {
     return videoFrame.toSMPTE();
   };
+
+  $scope.loadVideo = function() {
+    $scope.videoUrl = $sce.trustAsResourceUrl($scope.video.path);
+  };
+
+})
+
+.directive('fileInput', function ($parse) {
+    return {
+        restrict: "EA",
+        template: "<input type='file' />",
+        replace: true,          
+        link: function (scope, element, attrs) {
+           
+            var modelGet = $parse(attrs.fileInput);
+            var modelSet = modelGet.assign;
+            var onChange = $parse(attrs.onChange);
+            
+            var updateModel = function () {
+                scope.$apply(function () {
+                    modelSet(scope, element[0].files[0]);
+                    onChange(scope);
+                });                    
+            };
+            
+            element.bind('change', updateModel);
+        }
+    };
 });
